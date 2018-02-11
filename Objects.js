289909869ccd4fc6f49951394a895a805bcf02a8
Esraa <israma7i@gmail.com>
2018-02-11 10:24:21 +0200

    “anycommentyouWant”

--- Objects.js
+++ Objects.js
@@ -5,8 +5,23 @@
 	// -nationality
 	// and then save theme in one array called mates!
 
+var mates = [
+{name:{firstName:"shefaa", secondName:"almostafaa"}, age:17, favoriteSports: "tennis", nationality: "Syria"},
+{name:{firstName:"samer", secondName:"salmeh"}, age:25, favoriteSports: "wrustling", nationality: "Syria"},
+{name:{firstName:"safwan", secondName:"almoukdad"}, age:27, favoriteSports: "football", nationality: "jordan"},
+{name:{firstName:"khadija", secondName:"almostafaa"}, age:19, favoriteSports: "basketball", nationality: "Syria"},
+{name:{firstName:"ahmad", secondName:"alzoubay"}, age:26, favoriteSports: "pingball", nationality: "Syria"}
+	]
 // 2-depending on the previous exercises , write a function that returns the older classmate from mates array.
+function older(arr){
+	for(var i=0;arr[i]<arr[arr.length];i++){
+		var age=[];
+		for(var key in arr){
+        age.
+		}
 
+	}
+}
 // 3-write a function that takes an object as argument and returns how many keys does that object contain.
 	 nbOfObjKeys({}) // ==> 0
 	 nbOfObjKeys({name:"housam",age:"3"}) // ==> 2
