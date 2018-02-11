ed325dd20de0ce45552197912107fd0c64f6a40c
Fatima <en.fh90@gmail.com>
2018-02-11 07:26:45 +0200

    init

---
+++ Objects.js
@@ -0,0 +1,17 @@
+// 1-define objects for 5 of your classmater containing the following keys:
+	// -full name 
+	// -age 
+	// -favorite sports 
+	// -nationality
+	// and then save theme in one array called mates!
+
+// 2-depending on the previous exercises , write a function that returns the older classmate from mates array.
+
+// 3-write a function that takes an object as argument and returns how many keys does that object contain.
+	 nbOfObjKeys({}) // ==> 0
+	 nbOfObjKeys({name:"housam",age:"3"}) // ==> 2
+
+// if you finished the above, solve the following:
+// write a function that takes array of strings and returns an array of the strings that have the same length
+	    ['hi','hello','welcome','hy'] // ==> ["hi","hy"]
+
