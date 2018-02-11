caf520baf36f0d2c4cf58d75a657376b6cf73316
AlaqraAlik <alikalaqra@gmail.com>
2018-02-11 00:14:16 -0800

    alik toy1

--- Objects.js
+++ Objects.js
@@ -15,3 +15,71 @@
 // write a function that takes array of strings and returns an array of the strings that have the same length
 	    ['hi','hello','welcome','hy'] // ==> ["hi","hy"]
 
+
+
+var mate1=
+{
+	fullName:"alik essa alaqra",
+	age:24,
+	favSport:"football",
+	nationality:"jordanian"
+
+};
+
+var mate2=
+{
+	fullName:"ahmad zeyad salameh",
+	age:33,
+	favSport:"walking",
+	nationality:"palestanian"
+
+};
+var mate3=
+{
+	fullName:"sleman nayef rusan",
+	age:27,
+	favSport:"running ",
+	nationality:"canadian"
+
+};
+var mate4=
+{
+	fullName:"ibrahem riyad jawdat",
+	age:24,
+	favSport:"football",
+	nationality:"jordanian"
+
+};var mate5=
+{
+	fullName:"moath waleed alnajar",
+	age:26,
+	favSport:"basketball",
+	nationality:"ukranian"
+
+};
+
+
+var arr=[mate1,mate2,mate3,mate4,mate5];
+
+
+function older(arr)
+{
+    var old=0;
+    var count=arr.length
+      {for(var i=0;i<count;i++)
+      
+
+    {	
+    	if(arr[i].age>old) 
+
+    	old=arr[i].age
+    {
+    }
+    return old;	
+
+
+
+    
+
+
+}
