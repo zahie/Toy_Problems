d6bbb8823dd121a4aa46fe83ad7e6304f907034c
ShSukkar <sukkarsmiling89@hotmail.com>
2018-02-11 10:26:03 +0200

    “anycommentyouWant”

--- Objects.js
+++ Objects.js
@@ -4,14 +4,100 @@
 	// -favorite sports 
 	// -nationality
 	// and then save theme in one array called mates!
+	var mates = [];
+
+	var obj1 = {
+		fullName: "Alaa",
+		age: 28,
+		sports: ["walking"],
+		nationality: "Jordanian"
+	};
+	var obj2 = {
+		fullName: "soso",
+		age: 27,
+		sports: ["swimming"],
+		nationality: "Lebanese"
+	};
+	var obj3 = {
+		fullName: "lolo",
+		age: 22,
+		sports: ["basket ball", "walking"],
+		nationality: "Syrian"
+	};
+	var obj4 = {
+		fullName: "Lama",
+		age: 23,
+		sports: ["swimming"],
+		nationality: "Jordanian"
+	};
+	var obj5 = {
+		fullName: "roro",
+		age: 31,
+		sports: ["foot ball"],
+		nationality: "Palestanian"
+	};
+
+
+	mates.push(obj1);
+	mates.push(obj2);
+	mates.push(obj3);
+	mates.push(obj4);
+	mates.push(obj5);
+	
+
 
 // 2-depending on the previous exercises , write a function that returns the older classmate from mates array.
+function findOlder(arr)
+{
+	var oldest = arr[0].age;
+	var oldClassmate = {};
 
+	for(var i=1; i<arr.length; i++)
+	{
+		if(arr[i].age > oldest)
+		{
+			oldest = arr[i].age;
+			oldClassmate = arr[i];
+		}
+	}
+
+	return oldClassmate;
+}
 // 3-write a function that takes an object as argument and returns how many keys does that object contain.
 	 nbOfObjKeys({}) // ==> 0
 	 nbOfObjKeys({name:"housam",age:"3"}) // ==> 2
+function nbOfObjKeys(obj)
+{
+	var count =0;
+
+	for(var key in obj)
+	{
+		count++;
+	}
+
+	return count;
+}
 
 // if you finished the above, solve the following:
 // write a function that takes array of strings and returns an array of the strings that have the same length
 	    ['hi','hello','welcome','hy'] // ==> ["hi","hy"]
+function findSameLength(arr)
+{
+	var length;
+	var count = 0
+	var arr2 = [];
 
+	for(var i=0; i< arr.length; i++)
+	{
+		length = arr1[i].length;
+		for(var j=0; j<arr1.length; j++)
+		{
+			if(length === arr1[j].length)
+			{
+				arr2.push(arr1[j]);
+			}
+		}
+		if(arr2.length >0)
+			break;
+	}
+}
