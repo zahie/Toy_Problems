5dd8f579f148053946251a570e4ac55947980a5f
MohammadJozaa7 <36266902+MohammadJozaa7@users.noreply.github.com>
2018-02-11 12:10:06 +0200

    “anycommentyouWant”

--- Objects.js
+++ Objects.js
@@ -5,13 +5,79 @@
 	// -nationality
 	// and then save theme in one array called mates!
 
-// 2-depending on the previous exercises , write a function that returns the older classmate from mates array.
+	var mates=
+	[
+     {
+     	fullName:  "mohammad jozaa"      ,  
+     	age:      23          ,
+     	favoriteSports:    "Football"          ,
+     	nationality:     "jordainan"
+     },
+       {
+       fullName:      "bayan saad"  ,  
+     	age:           20   ,
+     	favoriteSports:  "Basketball"           ,
+     	nationality:    "syrian"
+     },
+     {
+        fullName:"monther amer",  
+     	age:29,
+     	favoriteSports:"Football",
+     	nationality:"jordainan"
+     },
+     {
+        fullName:       "yusur alani",  
+     	age:              27,
+     	favoriteSports:     "Basketball",
+     	nationality:    "jordainan"
+     },
+     {
+        fullName:      "abdulhameed abdullah",  
+     	age:               29,
+     	favoriteSports:    "Football",
+     	nationality:      "jordainan"
+     }
+	]
 
-// 3-write a function that takes an object as argument and returns how many keys does that object contain.
-	 nbOfObjKeys({}) // ==> 0
-	 nbOfObjKeys({name:"housam",age:"3"}) // ==> 2
+// 2-depending on the previous exercises , write a function 
+// that returns the older classmate from mates array.
+
+function older(ma)
+{
+var olderOne= {
+       fullName:      "bayan saad"  ,  
+        age:           20   ,
+        favoriteSports:  "Basketball"           ,
+        nationality:    "syrian"
+     }
+for (var i = 0 ; i < mates.lenght; i++) {
+    if (mates[i].age>olderOne.age) {
+    olderOne=mates[i]
+    }
+}
+return olderOne
+}
+
+// 3-write a function that takes an object 
+// as argument and returns how many keys 
+// does that object contain.
+	 //nbOfObjKeys({}) // ==> 0
+	// nbOfObjKeys({name:"housam",age:"3"}) // ==> 2
+
+nbOfObjKeys(obj){
+
+var x=0
+for (var i = Things.length - 1; i >= 0; i--) {
+    Things[i]
+}
+
+
+
+}
+
+
 
 // if you finished the above, solve the following:
 // write a function that takes array of strings and returns an array of the strings that have the same length
-	    ['hi','hello','welcome','hy'] // ==> ["hi","hy"]
+	  //  ['hi','hello','welcome','hy'] // ==> ["hi","hy"]
 
