36fce0425bf8d7be73893cb4f298606c6720fa43
MohammadAlRifai <mohammad94sar@gmail.com>
2018-02-11 11:44:18 +0200

    MohammadAlRifai

--- Objects.js
+++ Objects.js
@@ -5,11 +5,27 @@
 	// -nationality
 	// and then save theme in one array called mates!
 
+mates[{fullname="Mohammad",age=24,favoritesport="basketball",nationality="syrian"},
+{fullname="Abdulatuf",age=29,favoritesport="basketball",nationality="syrian"},
+{fullname="Ahmed",age=22,favoritesport="basketball",nationality="syrian"},
+{fullname="Mohammad",age=24,favoritesport="basketball",nationality="jordanian"},
+{fullname="Husen",age=30,favoritesport="basketball",nationality="jordanian"}
+	]
 // 2-depending on the previous exercises , write a function that returns the older classmate from mates array.
+function old(arr){
+	var x = [];
+	if age>29 {
+
+	}return x + obj
+}
 
 // 3-write a function that takes an object as argument and returns how many keys does that object contain.
 	 nbOfObjKeys({}) // ==> 0
 	 nbOfObjKeys({name:"housam",age:"3"}) // ==> 2
+
+	 function nbOfObjKeys(obj){
+
+	 }
 
 // if you finished the above, solve the following:
 // write a function that takes array of strings and returns an array of the strings that have the same length
