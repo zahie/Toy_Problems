eb5ed15be4161a46b89448a71e57f73c54895694
RBK6 <rbk6@RBK6s-Mac-mini.local>
2018-02-11 10:15:02 -0500

    “anycommentyouWant”

--- Objects.js
+++ Objects.js
@@ -5,6 +5,7 @@
 	// -nationality
 	// and then save theme in one array called mates!
 
+
 // 2-depending on the previous exercises , write a function that returns the older classmate from mates array.
 
 // 3-write a function that takes an object as argument and returns how many keys does that object contain.
@@ -14,4 +15,73 @@
 // if you finished the above, solve the following:
 // write a function that takes array of strings and returns an array of the strings that have the same length
 	    ['hi','hello','welcome','hy'] // ==> ["hi","hy"]
+// solution 1 
+var obj1={ 
+	fullName:'MaisMuhtaseb',                 
+	age:31,
+	favSpo:'racket',
+	nationality:'jordanian',
+
+};
+var obj2={ 
+	fullName:'Nada',
+	age:32,
+	favSpo:'football',
+	nationality:'yameni',
+};
+var obj3={ 
+	fullName:'SaraKoki',
+	age:23,
+	favSpo:'football',
+	nationality:'syrian',
+	
+
+};
+var obj4={ 
+	fullName:'abdelhamed',
+	age:28,
+	favSpo:'football',
+	nationality:'jordanian',
+	
+
+};
+var obj5={ 
+	fullName:'salsabeel',
+	age:23,
+	favSpo:'football',
+	nationality:'jordanian',
+	
+
+};
+var objArray=[obj1,obj2,obj3,obj4,obj5];
+
+// solution 2 
+function OldestAge(objArray){
+	var max=objArray[0].age;
+	for(var i=1;i<objArray.length;i++){
+		if (max<objArray[i].age;){
+			max=objArray[i].age;
+		}
+	}
+	return max
+}
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
