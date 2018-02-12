3006a18b05e51b1e1317e07fce9b36281772b0e0
BayanSaad <bayan.saad997@gmail.com>
2018-02-12 10:22:36 +0200

    “anycommentyouWant”

--- Data_Modeling.js
+++ Data_Modeling.js
@@ -7,3 +7,64 @@
 	// 	-calculate the number of male friends that your class have by writing a function called nbOfMale.
 	// 	-Write a function searchMates that, given a query and an array of Mates,
 	//           searche the array of mates for "matching" mate. You will decide what way you want to write in your search algorithm.
+
+//q1 
+var classmates=[
+{
+
+	firstName:"mohamad",
+	lastName:"saad",
+	age:23,
+	job:"teacher",
+
+};
+var classmates1{
+	firstName:"ali",
+	lastName:"suker",
+	age:33,
+	job:"vet",
+
+};
+var classmates3{
+firstName:"alia",
+	lastName:"shaker",
+	age:22,
+	job:"doctor",
+
+}]
+var array=[]
+function classmets(c,s,f,j){
+
+
+return
+	
+{
+	firstNames:c,
+	lastName:s,
+	age:f,
+	job:j
+
+
+
+}
+
+
+
+
+function displayFriend(mate){
+
+return 
+
+}
+
+
+
+
+
+function addFriend(mate){
+	return
+}
+
+function searchMates(query,array){
+
+}
