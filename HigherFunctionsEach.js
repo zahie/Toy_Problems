b7614bf1250f87a47f1add26e1d8fa2d09559102
Waed93 <alshyabwaed@gmail.com>
2018-02-13 09:39:02 -0500

    this is for HigherFunctionsEach.js

---
+++ HigherFunctionsEach.js
@@ -0,0 +1,39 @@
+	// 1) 'absAll' create function that accept array as input and check the element 
+    //     if it's negative make it positive 
+    function each(array, func) {
+        for (var i = 0; i < array.length; i++) {
+          func(array[i]);
+        }
+    }
+	function absAll(array) {
+	 each(array,function(array){
+	 	if (array<0){
+	 		array*=-1;
+	 	}
+	 })
+	 return array;
+	}
+
+	/*
+	2) using improved each write function that multiply each element in array with
+	 4 if index multiply of 4 
+
+	 var x = [2, 4, 6, 8, 10, 12, 14, 16]
+		multiplyOfFour(x);  => [16, 64 , 256] 
+    */
+   
+    function each(array, func) {
+        for (var i = 0; i < array.length; i++) {
+            func(array[i], i);
+        }
+    }
+	function multiplyOfFour(array) {
+		var result=[];
+		each(array,function(array,i){
+			if(array%4===0){
+				result.push(array*4)
+
+			}
+		})
+	    return result
+	}
